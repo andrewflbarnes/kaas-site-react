62fc5193e1f62e2129ebd13f1871d4d8e949dc5e
andrewflbarnes <andrew.barnes2@mastercard.com>
2019-05-11 02:09:08 +0100

    Improve responsive sizing of filter accept and cancel buttons

--- src/DropdownOptions/DropdownOptions.js
+++ src/DropdownOptions/DropdownOptions.js
@@ -13,9 +13,9 @@ export default function DropdownOptions({
       <DropdownSelect type="Competition" list={competitions} toggleItem={handleCompetitionToggle} current={competition}/>
       <DropdownSelect type="Season" list={seasons} toggleItem={handleSeasonToggle} current={season}/>
       <DropdownSelect type="League" list={leagues} toggleItem={handleLeagueToggle} current={league}/>
-      <div className="col-4 offset-4">
-        <Button className="col-5 mx-1" variant="outline-success" onClick={handleApplyFilters}>Apply</Button>
-        <Button className="col-5 mx-1" variant="outline-danger" onClick={handleCancelFilters}>Cancel</Button>
+      <div className="col-md-4 offset-md-4">
+        <Button className="col-3 col-md-5 mx-1" variant="outline-success" onClick={handleApplyFilters}>Apply</Button>
+        <Button className="col-3 col-md-5 mx-1" variant="outline-danger" onClick={handleCancelFilters}>Cancel</Button>
       </div>
     </>
   )
